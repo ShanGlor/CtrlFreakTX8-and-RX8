2ad1746e3310e62810dcaee0d4033580202b8e89
Shan <pro_unholy@yahoo.com>
2017-09-27 11:15:38 +0800

    Add files via upload

---
+++ CtrlFreakTx8/ppm.h
@@ -0,0 +1,55 @@
+//////////////////////PPM OUT CONFIGURATION///////////////////////////////
+#define channel_number 8  //set the number of chanels
+#define default_servo_value 1500  //set the default servo value
+#define PPM_FrLen 27000  //set the PPM frame length in microseconds (1ms = 1000µs)  27000//22500/ 38.5ms   set frame length at 26.5ms, polarity at positive and delay at 300us.
+#define PPM_PulseLen 400  //set the pulse length  400//300
+#define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 4  //set PPM signal output pin on the arduino  D4
+
+int ppm[channel_number];
+
+void setupPPM() {
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, 0);  //set the PPM signal pin to the default state (off)
+
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+
+  OCR1A = 100;  // compare match register (not very important, sets the timeout for the first interrupt)
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+}
+
+ISR(TIMER1_COMPA_vect){  //leave this alone
+  static boolean state = true;
+  
+  TCNT1 = 0;
+  
+  if(state) {  //start pulse
+    digitalWrite(sigPin, onState);
+    OCR1A = PPM_PulseLen * 2;
+    state = false;
+  }
+  else{  //end pulse and calculate when to start the next pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+  
+    digitalWrite(sigPin, !onState);
+    state = true;
+
+    if(cur_chan_numb >= channel_number){
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PPM_PulseLen;// 
+      OCR1A = (PPM_FrLen - calc_rest) * 2;
+      calc_rest = 0;
+    }
+    else{
+      OCR1A = (ppm[cur_chan_numb] - PPM_PulseLen) * 2;
+      calc_rest = calc_rest + ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
+}
